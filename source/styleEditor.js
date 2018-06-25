5c3aecbc3f7d6914766cddcbb6b636c6b1e643db
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-25 01:12:18 -0600

    Completes styler

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
   // your code goes here.
+  $('form#style_editor').submit(event => {
+    event.preventDefault()
+    let selector = $('input[name="selector"]').val()
+    let property = $('input[name="property"]').val()
+    let value = $('input[name="value"]').val()
+
+    $(selector).css(property, value)
+  })
 });
